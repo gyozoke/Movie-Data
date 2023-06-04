15c3de13c6ec3424e06cb2b4c883d4395eb441c5
Victor Gyozo Vancsura <gyozovancsura@gmail.com>
2023-06-04 13:45:09 +0100

    added more styling and functionality, including a sort method and create a new movie

--- index.js
+++ index.js
@@ -43,7 +43,9 @@ let movieData = {
 	},
   };
 
-  //Create a movieData array
+  
+//Render the Data on the webpage
+//Create a movieData array
 var movieDataArray = Object.keys(movieData).map(key => {
     let movieArray = movieData[key];
     movieArray.title = key
@@ -75,27 +77,120 @@ var movieDataArray = Object.keys(movieData).map(key => {
     // Append newyly created card element to the container
     container.innerHTML += content;
   });
+  
 
+//sort the movieDataArray by title in both ascending and descending order using a single button
+//First, you need to define a variable to keep track of
+//the current sorting order. Let's call it ascending and set it to true initially.
+let ascending = true;
 
+//Then we can add an event listener to the button that will trigger
+//the sorting function when clicked. Inside the event listener,
+//you can use the sort() method to sort the movieDataArray based on the title property.
+const sortButton = document.getElementById("sortButton");
+sortButton.addEventListener("click", () => {
+  movieDataArray.sort((a, b) => {
+    const titleA = a.title.toLowerCase();
+    const titleB = b.title.toLowerCase();
+    if (titleA < titleB) return ascending ? -1 : 1;
+    if (titleA > titleB) return ascending ? 1 : -1;
+    return 0;
+  });
 
-const sort_title_btn = document.querySelector('.sort-title');
+  ascending = !ascending; // Toggle the sorting order
 
-let desc = false;
-sort_title_btn.addEventListener('click', () => {
-    let array = sort_array_by(movieDataArray, 'movie.title', desc());
-    displayList(array);
-
-    desc = !desc;
+  // Update the movie list in the container
+  updateMovieList();
 });
 
-function sort_array_by(array, sort, desc) {
-    array.sort(function (a, b) {
-        if (a[sort] < b[sort]) return -1;
-        if (a[sort] > b[sort]) return 1;
-        return 0;
+//Finally, you can create a separate function, updateMovieList(),
+//that will update the HTML content of the container with the sorted movie list.
+function updateMovieList() {
+  container.innerHTML = ""; // Clear the container
+
+  movieDataArray.forEach((movie) => {
+    const content = `
+    <div class="card">
+      <div class="card-header">
+        <h2>${movie.title}</h2>
+      </div>
+      <div class="card-body">
+        <h3>Plot</h3>
+        <p>${movie.plot}</p>
+        <h3>Cast</h3>
+        <p>${movie.cast.join(", ")}</p>
+        <h3>Rating</h3>
+        <p>${movie.rating}</p>
+        <h3>Runtime</h3>
+        <p>${movie.runtime}</p>
+        <h3>Year</h3>
+        <p>${movie.year}</p>
+      </div>
+    </div>
+    `;
+    container.innerHTML += content;
+  });
+}
+
+
+//Create new movie
+  document.getElementById('movieForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Get input values
+    const title = document.getElementById('titleInput').value;
+    const plot = document.getElementById('plotInput').value;
+    const cast = document.getElementById('castInput').value.split(', ');
+    const rating = parseFloat(document.getElementById('ratingInput').value);
+    const runtime = document.getElementById('runtimeInput').value;
+    const year = parseInt(document.getElementById('yearInput').value);
+  
+    // Create new movie object
+    const newMovie = {
+      title: title,
+      plot: plot,
+      cast: cast,
+      rating: rating,
+      runtime: runtime,
+      year: year
+    };
+  
+    // Push new movie object to movieDataArray
+    movieDataArray.push(newMovie);
+  
+    // Reset form fields
+    document.getElementById('movieForm').reset();
+  
+    // Call function to update the movie cards on the page
+    updateMovieCards();
+  });
+  
+
+  function updateMovieCards() {
+    const newContainer = document.getElementById('container');
+    newContainer.innerHTML = ''; // Clear previous movie cards
+  
+    movieDataArray.forEach((movie) => {
+      const content = `
+        <div class="card">
+          <div class="card-header">
+            <h2>${movie.title}</h2>
+          </div>
+          <div class="card-body">
+            <h3>Plot</h3>
+            <p>${movie.plot}</p>
+            <h3>Cast</h3>
+            <p>${movie.cast.join(", ")}</p>
+            <h3>Rating</h3>
+            <p>${movie.rating}</p>
+            <h3>Runtime</h3>
+            <p>${movie.runtime}</p>
+            <h3>Year</h3>
+            <p>${movie.year}</p>
+          </div>
+        </div>
+      `;
+      newContainer.innerHTML += content;
     });
-
-    if (desc) array.reverse();
-
-    return array;
-};
+  }
+  
